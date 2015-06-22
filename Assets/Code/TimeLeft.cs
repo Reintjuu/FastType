4c082951b04bf79117283b38d2ec396079fa6997
Reinier de Vries <reiniervries@outlook.com>
2015-06-22 12:26:18 +0200

    Added a basic timer

---
+++ Assets/Code/TimeLeft.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Diagnostics;
+
+public class TimeLeft : MonoBehaviour
+{
+	Text textField;
+	Stopwatch stopWatch;
+	bool started;
+
+	// Use this for initialization
+	void Start()
+	{
+		textField = GetComponent<Text>();
+		stopWatch = new Stopwatch();
+	}
+
+	public void StartTimer()
+	{
+		stopWatch.Start();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (stopWatch.IsRunning)
+		{
+			textField.text = "Time: " + (60000 - stopWatch.ElapsedMilliseconds);
+		}
+	}
+}
